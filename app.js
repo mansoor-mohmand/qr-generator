6520ffc3d70972bb8fbd873a200a5f07b8d839c2
dev-mansoor <m.mohmand6@gmail.com>
2022-11-19 20:23:01 +0100

    testing 2

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 // https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=WIFI%3AT%3AWPA;S:KPNbox12;P:Welkom1348!; wifi qr code
 
-// const IMG_AFG  = document.querySelector("#IMG_AFG");
+const IMG_AFG  = document.querySelector("#IMG_AFG");
 const FILE_INPUT = document.getElementById("FILE_INPUT");
 const SHARE_BTN = document.getElementById("SHARE_BTN");
 
 const OUTPUT = document.getElementById("OUTPUT");
 
+
 // let files = IMG_AFG.src;
 // console.log(files);
 
+// SHARE_BTN.addEventListener("click",()=>{
+
+//     // const files = FILE_INPUT.files[0];
+
+//     const files = {
+//         name:IMG_AFG.src,
+//         type:"image/jpg"
+//     }
+//     console.log(files);
+
+    
+
+
+// });
+
+
+
 
 SHARE_BTN.addEventListener("click", async ()=>{
-    const files = FILE_INPUT.files;
+    // const files = FILE_INPUT.files;
+    const files = {
+        name:IMG_AFG.src,
+        type:"image/jpg"
+    }
     
     if (!navigator.canShare) {
         OUTPUT.textContent = "Not supported canShare";
